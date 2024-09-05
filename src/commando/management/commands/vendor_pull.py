8e145040f53e082ac95bcc6efa84230fa6665064
sruthijaganmohan <sruthi12jagan@gmail.com>
2024-09-05 18:33:25 +0530

    collecting vendor static files and saving them locally

---
+++ src/commando/management/commands/vendor_pull.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from typing import Any
+from django.conf import settings
+import helpers
+
+STATICFILES_VENDOR_DIR = getattr(settings, 'STATICFILES_VENDOR_DIR')
+
+VENDOR_STATICFILES = {
+    "flowbite.min.css": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css",
+    "flowbite.min.js": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js",
+}
+
+class Command(BaseCommand):
+
+    def handle(self, *args: Any, **options: Any):
+        self.stdout.write("Downloading vendor static files")
+        completed_urls = []
+        for name, url in VENDOR_STATICFILES.items():
+            out_path = STATICFILES_VENDOR_DIR / name
+            dl_success = helpers.download_to_local(url, out_path)
+            print(name, url, out_path)
+            if dl_success:
+                completed_urls.append(url)
+            else:
+                self.stdout.write(
+                    self.style.ERROR(f"Failed to download {url}")
+                )
+        if set(completed_urls) == set(VENDOR_STATICFILES.values()):
+            self.stdout.write(
+                self.style.SUCCESS("Successfully updated vendor static files")
+            )
+        else:
+            self.stdout.write(
+                self.style.WARNING("Some files are not updated")
+            )
--- src/core/settings.py
+++ src/core/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'visits', 
+    'visits',
+    'commando', 
 ]
 
 MIDDLEWARE = [
@@ -138,6 +139,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+# sources for python manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+# output for python manage.py collectstatic
+# local cdn
+STATIC_ROOT = BASE_DIR / "local-cdn"
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / "prod-cdn"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
